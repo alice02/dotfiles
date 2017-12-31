2829e3214a72aba462ec10c497edc3223f833276
alice02 <mk.alice02@gmail.com>
2017-12-31 15:57:36 +0900

    Add some emacs settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -339,3 +339,70 @@
  '(web-mode-css-property-name-face ((t (:foreground "#87CEEB"))))
  '(web-mode-css-string-face        ((t (:foreground "#D78181"))))
  )
+
+(setq company-dabbrev-downcase nil)
+
+;; multiple cursors
+(require 'multiple-cursors)
+(require 'smartrep)
+(declare-function smartrep-define-key "smartrep")
+(global-set-key (kbd "C-M-c") 'mc/edit-lines)
+(global-set-key (kbd "C-*")   'mc/mark-all-like-this)
+(global-unset-key "\C-q")
+(smartrep-define-key global-map "C-q"
+  '(("C-p"      . 'mc/mark-previous-like-this)
+    ("C-n"      . 'mc/mark-next-like-this)
+    ("u" . mc/unmark-next-like-this)
+    ("U" . mc/unmark-previous-like-this)
+    ("s" . mc/skip-to-next-like-this)
+    ("S" . mc/skip-to-previous-like-this)
+    ("*"        . 'mc/mark-all-like-this)))
+
+;; --------------------------------------------------
+;; ruby-mode
+;; http://shibayu36.hatenablog.com/entry/2013/03/18/192651
+;; --------------------------------------------------
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode)) ;; shebangがrubyの場合、ruby-modeを開く
+
+;; ruby-modeのインデントを改良する
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+;; --------------------------------------------------
+;; ruby-end
+;; endや括弧などを自動挿入する
+;; http://blog.livedoor.jp/ooboofo3/archives/53748087.html
+;; --------------------------------------------------
+(require 'ruby-end)
+(add-hook 'ruby-mode-hook
+  '(lambda ()
+    (abbrev-mode 1)
+    (electric-pair-mode t)
+    (electric-indent-mode t)
+    (electric-layout-mode t)))
+
+;; --------------------------------------------------
+;; ruby-block
+;; endにカーソルを合わせると、そのendに対応する行をハイライトする
+;; --------------------------------------------------
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
